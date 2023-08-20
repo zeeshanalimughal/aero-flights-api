dc41d293edcac73b05ec7bb720c6ecc9b9cef76b
zeeshanali <zeeshanaliryk18@gmail.com>
2023-08-20 13:37:22 +0500

    add new queryCall file, handle query parameters as well

---
+++ queryCall.js
@@ -0,0 +1,427 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+const port = process.env.PORT || 4000;
+const flightAwareApiBaseUrl = process.env.AERO_API_BASE_URL;
+// Middleware to parse JSON
+app.use(express.json());
+
+// Function to call FlightAware API
+async function callFlightAwareApi(
+  path,
+  headers = {
+    "x-apikey": process.env.AERO_FLIGHT_API_KEY,
+  }
+) {
+  try {
+    const response = await axios.get(`${flightAwareApiBaseUrl}${path}`, {
+      headers,
+    });
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+}
+
+// Define routes
+
+// Flights
+app.get("/flights/:id/map", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/flights/:id/position", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/flights/:id/route", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/flights/:id/track", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/flights/:ident", async (req, res) => {
+  console.log(req.url);
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/flights/:ident/canonical", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/flights/search", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/flights/search/advanced", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/flights/search/count", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/flights/search/positions", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Foresight
+app.get("/foresight/flights/:id/position", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/foresight/flights/:ident", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/foresight/flights/search/advanced", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Airports
+app.get("/airports", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/airports/:id", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/airports/:id/canonical", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/airports/:id/delays", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/airports/:id/flights", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Add more routes here...
+
+// Operators
+app.get("/operators", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/operators/:id", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/operators/:id/canonical", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/operators/:id/flights", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Add more routes here...
+
+// History
+app.get("/history/aircraft/:registration/last_flight", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/history/flights/:id/map", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/history/flights/:id/route", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/history/flights/:id/track", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/history/flights/:ident", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Add more routes here...
+
+// Miscellaneous
+app.get("/aircraft/:ident/owner", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/aircraft/types/:type", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/disruption_counts/:entity_type", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/disruption_counts/:entity_type/:id", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/schedules/:date_start/:date_end", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Alerts
+// Alerts
+app.get("/alerts", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/alerts", async (req, res) => {
+  const alertData = req.body;
+  try {
+    const response = await axios.post(
+      `${flightAwareApiBaseUrl}/alerts`,
+      alertData
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/alerts/:id", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put("/alerts/:id", async (req, res) => {
+  const { id } = req.params;
+  const alertData = req.body;
+  try {
+    const response = await axios.put(
+      `${flightAwareApiBaseUrl}/alerts/${id}`,
+      alertData
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.delete("/alerts/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const response = await axios.delete(
+      `${flightAwareApiBaseUrl}/alerts/${id}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/alerts/endpoint", async (req, res) => {
+  try {
+    const data = await callFlightAwareApi(req.url);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put("/alerts/endpoint", async (req, res) => {
+  const endpointData = req.body;
+  try {
+    const response = await axios.put(
+      `${flightAwareApiBaseUrl}/alerts/endpoint`,
+      endpointData
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.delete("/alerts/endpoint", async (req, res) => {
+  try {
+    const response = await axios.delete(
+      `${flightAwareApiBaseUrl}/alerts/endpoint`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhot:${port}`);
+});
